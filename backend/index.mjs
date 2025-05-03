9d33416d068c69f275e0bd512899f089d7bf0060
anumarif833 <anumarif833@gmail.com>
2025-05-03 20:43:06 +0500

    add vercel url

--- backend/index.mjs
+++ backend/index.mjs
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
 	cors({
-		origin: ['http://localhost:5174', 'http://localhost:5173',],
+		origin: ['http://localhost:5174', 'http://localhost:5173','https://anum-project-deplpy.vercel.app'],
 		methods: ['GET', 'PUT', 'POST', 'DELETE'],
 		credentials: true,
 		allowedHeaders: ['Content-Type', 'Authorization'],
